8b00b1c641b34019350bd84894496b39066e1ca4
AngelCityBusiness <158473688+AngelCityBusiness@users.noreply.github.com>
2024-04-23 22:33:39 -0700

    Create ![tempalte_tawk.to]FILE_NAME.js

---
+++ ![tempalte_tawk.to]FILE_NAME.js
@@ -0,0 +1,50 @@
+// FILE_NAME_HERE.js
+
+// MAKE SURE TO END YOUR FILE NAME IN ".js" !!
+// I recommend a file name like "#####_business_name.js"
+
+(function() {
+
+    
+    // Load the chat plugin
+    var script = document.createElement('script');
+    
+    script.src = 'https://app.aminos.ai/js/chat_plugin.js'; // Regular website Original Script. // Comment out if not using.
+    //script.src = 'https://app.aminos.ai/js/chat_plugin_ghl.js'; // GoHighLevel Original Script. // Comment out if not using.
+    //script.src = 'https://app.aminos.ai/js/chat_form_plugin.js'; // Inline Website Original Script. // Comment out if not using.
+    
+    script.setAttribute('data-bot-id', '#####'); // Setting the bot ID
+    document.head.appendChild(script);
+
+    // Add custom CSS styling
+    var css = `div.talktext p {
+        color: black !important;
+        font-family: sans-serif !important;
+    }`;
+    var style = document.createElement('style');
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+    document.head.appendChild(style);
+    
+    //************************************************
+    //NEW INTEGRATION CODE: <script src="https://code.angelcitymarketing.com/FILE_NAME_HERE.js"></script>
+     //MAKE SURE TO ADD: <div id="chat_form"></div>
+     //WITHIN THE HTML BLOCK IF THE FILE IS FOR IFRAME EMBED
+
+    //Notice the Integration code does NOT include the repository name. The repo name is not necessary for the integration code.
+    //************************************************
+    
+    // Optionally, add any additional JavaScript code here
+
+    // Ensure the script runs in noConflict mode to avoid conflicts with other libraries
+     if (window.jQuery) {
+        var $ = jQuery.noConflict(true);
+        $(document).ready(function() {
+            // Additional jQuery-dependent code can be added here
+        });
+    }
+
+})();
